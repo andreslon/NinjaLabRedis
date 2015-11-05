b22bcba01d154a543d0b0a8a3ca33463aa18eba5
Andrés Londoño <andreslon@outlook.com>
2015-11-05 13:29:23 -0500

    Implementacion de controlador con su respectivas vistas y creacion de clase de conexion a Redis.

---
+++ NinjaLab.Redis/NinjaLab.Redis/Helpers/RedisHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using NinjaLab.Redis.Models;
+using StackExchange.Redis;
+
+namespace NinjaLab.Redis.Helpers
+{
+    public class RedisHelper
+    {
+        private static ConnectionMultiplexer redis;
+
+        public static IDatabase GetDatabase()
+        {
+            if (redis == null)
+            {
+                var server =
+                 "ninjalabredis.redis.cache.windows.net,abortConnect=false,ssl=true,password=hUIb6nVH9fqb3/xjgWNsLYZdSi9lhIPL4HxCIJIlD3E=";
+                redis = ConnectionMultiplexer.Connect(server);
+            }
+            return redis.GetDatabase();
+        }
+        public static Event GetEvent(Guid guid)
+        {
+            var db = GetDatabase();
+            var eventResult = db.StringGet(guid.ToString());
+            if (eventResult.IsNullOrEmpty)
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<Event>(eventResult);
+        }
+
+        public static void SetEvent(Event @event)
+        {
+            var db = RedisHelper.GetDatabase();
+            db.StringSet(@event.Id.ToString(), JsonConvert.SerializeObject(@event));
+        }
+
+        public static void DeleteEvent(Guid guid)
+        {
+            var db = RedisHelper.GetDatabase();
+            db.KeyDelete(guid.ToString());
+        }
+    }
+}
